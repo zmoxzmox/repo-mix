e4dc5bd9fea5a15fcb8930fb5088eac758411edb
SB <richstyles@gmail.com>
2026-07-17 21:10:14 +0900

    Fix main style gate

--- Sources/RepoPrompt/App/HelpMenu.swift
+++ Sources/RepoPrompt/App/HelpMenu.swift
@@ -2,35 +2,33 @@ import SwiftUI
 
 struct HelpMenu: View {
     var body: some View {
-        Group {
-            Link(
-                "What's New",
-                destination: URL(string: "https://repoprompt.com/docs#s=changelog")!
-            )
+        Link(
+            "What's New",
+            destination: URL(string: "https://repoprompt.com/docs#s=changelog")!
+        )
 
-            Button("Setup Guide…") {
-                NotificationCenter.default.post(name: .showAgentOnboardingWizard, object: nil)
-            }
-
-            Link(
-                "Getting Started",
-                destination: URL(string: "https://youtube.com/playlist?list=PLFg9suyZ1OnIKYyoCbAGBaFB-QOAk1nSq&si=hiUSja9eTRWeB26j")!
-            )
-
-            Link(
-                "Documentation",
-                destination: URL(string: "https://repoprompt.com/docs#s=getting-started")!
-            )
-
-            Link(
-                "Repo Prompt",
-                destination: URL(string: "https://repoprompt.com")!
-            )
-
-            Link(
-                "Discord Community",
-                destination: URL(string: "https://discord.gg/NtbFDAJPGM")!
-            )
+        Button("Setup Guide…") {
+            NotificationCenter.default.post(name: .showAgentOnboardingWizard, object: nil)
         }
+
+        Link(
+            "Getting Started",
+            destination: URL(string: "https://youtube.com/playlist?list=PLFg9suyZ1OnIKYyoCbAGBaFB-QOAk1nSq&si=hiUSja9eTRWeB26j")!
+        )
+
+        Link(
+            "Documentation",
+            destination: URL(string: "https://repoprompt.com/docs#s=getting-started")!
+        )
+
+        Link(
+            "Repo Prompt",
+            destination: URL(string: "https://repoprompt.com")!
+        )
+
+        Link(
+            "Discord Community",
+            destination: URL(string: "https://discord.gg/NtbFDAJPGM")!
+        )
     }
 }
